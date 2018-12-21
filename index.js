44126e6891023a8573b6219af7f5c7cff737a6ed
Denis Popov <deniolp@gmail.com>
2018-12-21 22:23:07 +0300

    Начальная подготовка проекта для изучения service workers

---
+++ index.js
@@ -0,0 +1,16 @@
+'use stricts';
+
+(function() {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('ws.js')
+      .then(function () {
+        navigator.serviceWorker.ready
+          .then(function (worker) {
+            worker.sync.register('syncdata');
+          })
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+})();
