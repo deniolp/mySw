87a4323f872587ccb9df72b14e829e6989b563de
Denis Popov <deniolp@gmail.com>
2018-12-22 09:46:10 +0300

    Подготовка livereload, чтобф не коммитить каждое небольшое изменение на сервер

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var gulp = require('gulp');
+var server = require('browser-sync').create();
+
+gulp.task('serve', function() {
+  server.init({
+    server: '/',
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch('source/*.html').on('change', server.reload);
+  gulp.watch("source/*.js").on("change", server.reload);
+});
