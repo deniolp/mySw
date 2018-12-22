caf83c85d6bfe8941e247318825cfb39d890372d
Denis Popov <deniolp@gmail.com>
2018-12-22 10:56:05 +0300

    Реализация подмены пути к сервисворкеру в зависимости от того, в каком протоколе открывается страница

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 'use stricts';
 
 (function() {
+  let path = '/mySw/sw.js';
+  if (location.host === 'localhost:3000') {
+    path = '/sw.js';
+  }
+
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/mySw/sw.js')
+    navigator.serviceWorker.register(path)
       .then(function (registration) {
         console.log('ServiceWorker registration successful with scope: ', registration.scope);
       })
